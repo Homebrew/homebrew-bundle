fc363e744858427d8785a8b06dada0ab7d69b99a
James Anderson <me@jamesaanderson.com>
2013-06-19 11:17:10 -0700

    Simplecov and Coveralls integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,11 @@ SimpleCov.start
 require 'coveralls'
 Coveralls.wear!
 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  Coveralls::SimpleCov::Formatter,
+  SimpleCov::Formatter::HTMLFormatter
+]
+
 require 'brewdler'
 
 RSpec.configure do |config|
