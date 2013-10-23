59d4399640572236c5909a9c98498550e32acadb
Plehanov Dmitriy <onemanstartup@gmail.com>
2013-10-24 00:45:30 +0400

    opening .brewfile, Brewfile

--- lib/brewdler/commands/install.rb
+++ lib/brewdler/commands/install.rb
@@ -2,17 +2,21 @@ module Brewdler::Commands
   class Install
     def self.run
       begin
-        file = File.read(File.join(Dir.pwd, "Brewfile"))
-        Brewdler::Dsl.new(file).process
+        Brewdler::Dsl.new(brewfile).process
       rescue Errno::ENOENT => e
         raise "No Brewfile found"
       rescue NameError
-        file = File.open(File.join(Dir.pwd, "Brewfile"))
-        file.find_all do |name|
+        brewfile.find_all do |name|
           name.chomp!
           Brewdler::BrewInstaller.install(name) if name.length > 0 && name !~ /^ *#/
         end
       end
+    end
+
+  private
+
+    def self.brewfile
+      File.read(Dir['{*,.*}{B,b}rewfile'].first)
     end
   end
 end
