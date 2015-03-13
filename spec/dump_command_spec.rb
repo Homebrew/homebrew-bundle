a196b0a4f8d28137e8f320c43445938dc2229be5
Xu Cheng <xucheng@me.com>
2015-03-13 20:07:00 +0800

    eliminate warning during the test
    This commit fixes the following warning when invoking `bundle exec rake`
    
        Could not find rake-10.4.2 in any of the sources (Bundler::GemNotFound)

--- spec/dump_command_spec.rb
+++ spec/dump_command_spec.rb
@@ -3,21 +3,19 @@ require "spec_helper"
 describe Brewdler::Commands::Dump do
   context "when files existed" do
     before do
-      allow(Brewdler).to receive(:brew_installed?).and_return(true)
-      allow(Brewdler).to receive(:cask_installed?).and_return(true)
       allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
       allow(ARGV).to receive(:force?).and_return(false)
     end
 
     it "raises error" do
-      expect { Brewdler::Commands::Dump.run }.to raise_error
+      expect do
+        Bundler.with_clean_env { Brewdler::Commands::Dump.run }
+      end.to raise_error
     end
   end
 
   context "when files existed and `--force` is passed" do
     before do
-      allow(Brewdler).to receive(:brew_installed?).and_return(true)
-      allow(Brewdler).to receive(:cask_installed?).and_return(true)
       allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
       allow_any_instance_of(Pathname).to receive(:file?).and_return(true)
       allow(ARGV).to receive(:force?).and_return(true)
@@ -26,7 +24,9 @@ describe Brewdler::Commands::Dump do
     it "doesn't raise error" do
       expect(FileUtils).to receive(:rm)
       expect_any_instance_of(Pathname).to receive(:write)
-      expect { Brewdler::Commands::Dump.run }.to_not raise_error
+      expect do
+        Bundler.with_clean_env { Brewdler::Commands::Dump.run }
+      end.to_not raise_error
     end
   end
 end
