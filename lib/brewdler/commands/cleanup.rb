d9925c184d4afc3431260fa0f74b8eed4ae7bf07
Mike McQuaid <mike@mikemcquaid.com>
2015-03-20 20:33:53 +0000

    cleanup: also cleanup taps.
    Let the cleanup step also uninstall taps that have been tapped.
    
    Closes #56

--- lib/brewdler/commands/cleanup.rb
+++ lib/brewdler/commands/cleanup.rb
@@ -2,23 +2,43 @@ module Brewdler::Commands
   class Cleanup
     def self.run
       formulae = formulae_to_uninstall
-      return if formulae.empty?
+      taps = taps_to_untap
       if ARGV.dry_run?
-        puts "Would uninstall:"
-        puts_columns formulae
+        if formulae.any?
+          puts "Would uninstall:"
+          puts_columns formulae
+        end
+
+        if taps.any?
+          puts "Would untap:"
+          puts_columns taps
+        end
       else
-        Kernel.system "brew", "uninstall", "--force", *formulae
-        puts "Uninstalled #{formulae.size} formula#{ formulae.size == 1 ? "" : "e"}"
+        if formulae.any?
+          Kernel.system "brew", "uninstall", "--force", *formulae
+          puts "Uninstalled #{formulae.size} formula#{ formulae.size == 1 ? "" : "e"}"
+        end
+
+        if taps.any?
+          Kernel.system "brew", "untap", *taps
+        end
       end
     end
 
     private
 
     def self.formulae_to_uninstall
-      dsl = Brewdler::Dsl.new(Brewdler.brewfile)
-      kept_formulae = dsl.process.entries.select { |e| e.type == :brew }.map(&:name)
+      @@dsl ||= Brewdler::Dsl.new(Brewdler.brewfile).process
+      kept_formulae = @@dsl.entries.select { |e| e.type == :brew }.map(&:name)
       current_formulae = `brew list`.split
       current_formulae - kept_formulae
+    end
+
+    def self.taps_to_untap
+      @@dsl ||= Brewdler::Dsl.new(Brewdler.brewfile).process
+      kept_taps = @@dsl.entries.select { |e| e.type == :repo }.map(&:name)
+      current_taps = `brew tap`.split
+      current_taps - kept_taps
     end
   end
 end
--- spec/cleanup_command_spec.rb
+++ spec/cleanup_command_spec.rb
@@ -3,22 +3,29 @@ require "spec_helper"
 describe Brewdler::Commands::Cleanup do
   context "read Brewfile and currently installation" do
     before do
-      allow(File).to receive(:read).and_return("brew 'a'\nbrew 'b'")
-      allow(Brewdler::Commands::Cleanup).to receive(:`).and_return("a\nc\nd")
+      allow(File).to receive(:read).and_return("tap 'x'\ntap 'y'\nbrew 'a'\nbrew 'b'")
+      allow(Brewdler::Commands::Cleanup).to receive(:`) do |arg|
+        case arg
+        when "brew list" then "a\nc\nd"
+        when "brew tap" then "x\nz"
+        end
+      end
     end
 
     it "computes which formulae to uninstall" do
       expect(Brewdler::Commands::Cleanup.formulae_to_uninstall).to eql(%w[c d])
+      expect(Brewdler::Commands::Cleanup.taps_to_untap).to eql(%w[z])
     end
   end
 
-  context "no formulae to uninstall" do
+  context "no formulae to uninstall and no taps to untap" do
     before do
       allow(Brewdler::Commands::Cleanup).to receive(:formulae_to_uninstall).and_return([])
+      allow(Brewdler::Commands::Cleanup).to receive(:taps_to_untap).and_return([])
+      allow(ARGV).to receive(:dry_run?).and_return(false)
     end
 
     it "does nothing" do
-      expect(ARGV).not_to receive(:dry_run?)
       expect(Kernel).not_to receive(:system)
       Brewdler::Commands::Cleanup.run
     end
@@ -27,6 +34,7 @@ describe Brewdler::Commands::Cleanup do
   context "there are formulae to uninstall" do
     before do
       allow(Brewdler::Commands::Cleanup).to receive(:formulae_to_uninstall).and_return(%w[a b])
+      allow(Brewdler::Commands::Cleanup).to receive(:taps_to_untap).and_return([])
       allow(ARGV).to receive(:dry_run?).and_return(false)
     end
 
@@ -36,16 +44,30 @@ describe Brewdler::Commands::Cleanup do
     end
   end
 
-  context "there are formulae to uninstall but passing with `--dry-run`" do
+  context "there are taps to untap" do
+    before do
+      allow(Brewdler::Commands::Cleanup).to receive(:formulae_to_uninstall).and_return([])
+      allow(Brewdler::Commands::Cleanup).to receive(:taps_to_untap).and_return(%w[a b])
+      allow(ARGV).to receive(:dry_run?).and_return(false)
+    end
+
+    it "untaps taps" do
+      expect(Kernel).to receive(:system).with(*%w[brew untap a b])
+      Brewdler::Commands::Cleanup.run
+    end
+  end
+
+  context "there are formulae to uninstall and taps to untap but passing with `--dry-run`" do
     before do
       allow(Brewdler::Commands::Cleanup).to receive(:formulae_to_uninstall).and_return(%w[a b])
+      allow(Brewdler::Commands::Cleanup).to receive(:taps_to_untap).and_return(%w[a b])
       allow(ARGV).to receive(:dry_run?).and_return(true)
     end
 
-    it "lists formulae" do
-      expect(Brewdler::Commands::Cleanup).to receive(:puts_columns).with(%w[a b])
+    it "lists formulae and taps" do
+      expect(Brewdler::Commands::Cleanup).to receive(:puts_columns).with(%w[a b]).twice
       expect(Kernel).not_to receive(:system)
-      expect { Brewdler::Commands::Cleanup.run }.to output(/Would uninstall:/).to_stdout
+      expect { Brewdler::Commands::Cleanup.run }.to output(/Would uninstall:.*Would untap:/m).to_stdout
     end
   end
 end
