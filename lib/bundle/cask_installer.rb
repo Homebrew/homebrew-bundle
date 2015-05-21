5aba2873402dac2f2b1ec89e72debadb9d2aa7a3
Xu Cheng <xucheng@me.com>
2015-05-21 17:47:13 +0800

    cask_installer: skip installed cask

--- lib/bundle/cask_installer.rb
+++ lib/bundle/cask_installer.rb
@@ -1,11 +1,24 @@
 module Bundle
   class CaskInstaller
     def self.install(name)
-      if Bundle.cask_installed?
-        Bundle.system "brew", "cask", "install", name
-      else
+      unless Bundle.cask_installed?
         raise "Unable to install #{name}. Homebrew-cask is not currently installed on your system"
       end
+
+      if installed_casks.include? name
+        puts "Skip to install #{name}" if ARGV.verbose?
+        return true
+      end
+
+      if (success = Bundle.system "brew", "cask", "install", name)
+        installed_casks << name
+      end
+
+      success
+    end
+
+    def self.installed_casks
+      @@installed_casks ||= `brew cask list -1`.split("\n").map { |cask| cask.chomp " (!)" }
     end
   end
 end
