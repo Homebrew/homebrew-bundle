dee1266d3bdc1e1fcaef5aa7a8f89d0dfb0854a4
Xu Cheng <xucheng@me.com>
2015-06-07 20:35:14 +0800

    simplify file write logic

--- spec/dump_command_spec.rb
+++ spec/dump_command_spec.rb
@@ -18,15 +18,14 @@ describe Bundle::Commands::Dump do
   context "when files existed and `--force` is passed" do
     before do
       allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
-      allow_any_instance_of(Pathname).to receive(:file?).and_return(true)
       allow(ARGV).to receive(:force?).and_return(true)
       allow(ARGV).to receive(:value).and_return(nil)
     end
 
     it "doesn't raise error" do
-      expect(FileUtils).to receive(:rm)
-      expect_any_instance_of(Pathname).to receive(:write)
-      expect_any_instance_of(Pathname).to receive(:realpath) { |p| p }
+      io = double("File", :write => true)
+      expect_any_instance_of(Pathname).to receive(:open).with("w") { |&block| block.call io }
+      expect(io).to receive(:write)
       expect do
         Bundler.with_clean_env { Bundle::Commands::Dump.run }
       end.to_not raise_error
