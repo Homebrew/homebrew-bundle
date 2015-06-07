aea915b76c52a97499062eb2b0d96d4e675fdb8a
Xu Cheng <xucheng@me.com>
2015-06-07 17:28:22 +0800

    support anytap

--- lib/bundle/repo_installer.rb
+++ lib/bundle/repo_installer.rb
@@ -1,10 +1,14 @@
 module Bundle
   class RepoInstaller
-    def self.install(name)
-      if Bundle.brew_installed?
-        Bundle.system "brew", "tap", name
-      else
+    def self.install(name, clone_target)
+      unless Bundle.brew_installed?
         raise "Unable to tap #{name}. Homebrew is not currently installed on your system"
+      end
+
+      if clone_target
+        Bundle.system "brew", "tap", name, clone_target
+      else
+        Bundle.system "brew", "tap", name
       end
     end
   end
--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 
 describe Bundle::Dsl do
   it "processes input" do
-    expect_any_instance_of(Bundle::Dsl).to receive(:tap).with('phinze/cask')
+    expect_any_instance_of(Bundle::Dsl).to receive(:tap).with('caskroom/cask')
+    expect_any_instance_of(Bundle::Dsl).to receive(:tap).with(
+      'telemachus/brew',
+      'https://telemachus@bitbucket.org/telemachus/brew.git'
+    )
     expect_any_instance_of(Bundle::Dsl).to receive(:brew).twice
     expect_any_instance_of(Bundle::Dsl).to receive(:cask).with('google-chrome')
-    Bundle::Dsl.new("tap 'phinze/cask'\nbrew 'git'\ncask 'google-chrome'\nbrew 'emacs', args: ['cocoa', 'srgb', 'with-gnutls']")
+    Bundle::Dsl.new <<-EOS
+      tap 'caskroom/cask'
+      tap 'telemachus/brew', 'https://telemachus@bitbucket.org/telemachus/brew.git'
+      brew 'git'
+      brew 'emacs', args: ['with-cocoa', 'with-gnutls']
+      cask 'google-chrome'
+    EOS
   end
 end
