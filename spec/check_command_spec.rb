65f05cc24b54ff630c2ab87820df7c2861208655
Mike McQuaid <mike@mikemcquaid.com>
2015-08-25 00:36:21 +0100

    check_command_spec: more mocking.

--- spec/check_command_spec.rb
+++ spec/check_command_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe Bundle::Commands::Check do
   context "when dependencies are satisfied" do
     it "does not raise an error" do
+      allow(Bundle).to receive(:brew_installed?).and_return(true)
       allow(Bundle::Commands::Check).to receive(:casks_to_install).and_return([])
       allow(Bundle::Commands::Check).to receive(:formulae_to_install).and_return([])
       allow(Bundle::Commands::Check).to receive(:taps_to_tap).and_return([])
@@ -12,6 +13,7 @@ describe Bundle::Commands::Check do
 
   context "when dependencies are not satisfied" do
     it "raises an error" do
+      allow(Bundle).to receive(:brew_installed?).and_return(true)
       allow_any_instance_of(Bundle::CaskDumper).to receive(:casks).and_return([])
       allow_any_instance_of(Bundle::BrewDumper).to receive(:formulae).and_return([])
       allow(Bundle::BrewInstaller).to receive(:upgradable_formulae).and_return([])
