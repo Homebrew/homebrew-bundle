7b9d274397ab46302d3bf4af2003851295d9e5e3
Liquidsoul <liquidsoul@liquidsoul.fr>
2015-10-26 22:00:46 +0100

    Add a dedicated test for cask with options

--- spec/caskinstaller_spec.rb
+++ spec/caskinstaller_spec.rb
@@ -4,9 +4,11 @@ describe Bundle::CaskInstaller do
   def do_install
     Bundle::CaskInstaller.install("google-chrome")
   end
+
   def do_install_with_args
     Bundle::CaskInstaller.install("firefox", args: {:appdir => "/Applications"})
   end
+
   context "when brew is not installed" do
     it "raises an error" do
       allow(Bundle).to receive(:brew_installed?).and_return(false)
@@ -37,8 +39,15 @@ describe Bundle::CaskInstaller do
         allow(Bundle::CaskInstaller).to receive(:installed_casks).and_return([])
         expect(Bundle).to receive(:system).with("brew", "install", "caskroom/cask/brew-cask").and_return(true)
         expect(Bundle).to receive(:system).with("brew", "cask", "install", "google-chrome").and_return(true)
-        expect(Bundle).to receive(:system).with("brew", "cask", "install", "firefox", "--appdir=/Applications").and_return(true)
         expect(do_install).to eq(true)
+      end
+
+      it "works fine with arguments" do
+        allow(Bundle).to receive(:cask_installed?).and_return(false, true)
+        allow(ARGV).to receive(:verbose?).and_return(false)
+        allow(Bundle::CaskInstaller).to receive(:installed_casks).and_return([])
+        expect(Bundle).to receive(:system).with("brew", "install", "caskroom/cask/brew-cask").and_return(true)
+        expect(Bundle).to receive(:system).with("brew", "cask", "install", "firefox", "--appdir=/Applications").and_return(true)
         expect(do_install_with_args).to eq(true)
       end
     end
