bb1e9d170e0839038a2f2bf553f71b0e1a9411e1
Mike McQuaid <mike@mikemcquaid.com>
2015-11-01 09:55:05 -0800

    dsl_spec: rework to improve coverage.

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -2,16 +2,8 @@ require "spec_helper"
 
 describe Bundle::Dsl do
   it "processes input" do
-    expect_any_instance_of(Bundle::Dsl).to receive(:tap).with("caskroom/cask")
-    expect_any_instance_of(Bundle::Dsl).to receive(:tap).with(
-      "telemachus/brew",
-      "https://telemachus@bitbucket.org/telemachus/brew.git",
-    )
-    expect_any_instance_of(Bundle::Dsl).to receive(:brew).twice
-    expect_any_instance_of(Bundle::Dsl).to receive(:cask).with("google-chrome")
-    expect_any_instance_of(Bundle::Dsl).to receive(:cask_args).with(:appdir => "/Applications")
-    expect_any_instance_of(Bundle::Dsl).to receive(:cask).with("firefox", :args => {:appdir => "~/my-apps/Applications"})
-    Bundle::Dsl.new <<-EOS
+    allow(ARGV).to receive(:verbose?).and_return(true)
+    dsl = Bundle::Dsl.new <<-EOS
       cask_args :appdir => "/Applications"
       tap 'caskroom/cask'
       tap 'telemachus/brew', 'https://telemachus@bitbucket.org/telemachus/brew.git'
@@ -20,6 +12,16 @@ describe Bundle::Dsl do
       cask 'google-chrome'
       cask 'firefox', args: { appdir: '~/my-apps/Applications' }
     EOS
+    expect(dsl.entries[0].name).to eql("caskroom/cask")
+    expect(dsl.entries[1].name).to eql("telemachus/brew")
+    expect(dsl.entries[1].options).to eql(:clone_target => "https://telemachus@bitbucket.org/telemachus/brew.git")
+    expect(dsl.entries[2].name).to eql("git")
+    expect(dsl.entries[3].name).to eql("emacs")
+    expect(dsl.entries[3].options).to eql(:args => ["with-cocoa", "with-gnutls"])
+    expect(dsl.entries[4].name).to eql("google-chrome")
+    expect(dsl.entries[4].options).to eql(:args => {:appdir=>"/Applications"})
+    expect(dsl.entries[5].name).to eql("firefox")
+    expect(dsl.entries[5].options).to eql(:args => {:appdir=>"~/my-apps/Applications"})
   end
 
   it "handles invalid input" do
