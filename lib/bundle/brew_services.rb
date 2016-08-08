502c9d92823515babfd3e01d494ede7bbe898ef4
Richard Primus <rprimus@gmail.com>
2016-08-08 20:11:02 +0100

    Add restart_service: true to Brewfile on full dump

--- lib/bundle/brew_services.rb
+++ lib/bundle/brew_services.rb
@@ -1,5 +1,16 @@
 module Bundle
   class BrewServices
+    attr_accessor :started_services
+
+    def initialize()
+      @started_services = []
+      started = `brew services list`.lines.grep(/started/)
+      for f in started
+        parts=f.split(/\s+/)
+        @started_services << Hash[:name => parts[0], :user => parts[2], :plist => parts[3]]
+      end
+    end
+
     def self.stop(name)
       started = `brew services list`.lines.grep(/^#{Regexp.escape(name)} +started/).any?
       return true unless started
@@ -8,6 +19,13 @@ module Bundle
 
     def self.restart(name)
       Bundle.system "brew", "services", "restart", name
+    end
+
+    def started?(name)
+      for svc in @started_services
+        return true if svc[:name] == name
+      end
+      return false
     end
   end
 end
