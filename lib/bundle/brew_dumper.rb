4cfb755945207558713a42552b399b979cab2da2
Richard Primus <rprimus@gmail.com>
2016-08-08 20:11:02 +0100

    Add restart_service: true to Brewfile on full dump
    Add suggestions from @MikeMcQuaid
    Use Emumerators to reduce traditional control

--- lib/bundle/brew_dumper.rb
+++ lib/bundle/brew_dumper.rb
@@ -17,12 +17,10 @@ module Bundle
 
     def self.dump
       formulae.map do |f|
-        if f[:args].empty?
-          "brew '#{f[:full_name]}'"
-        else
-          args = f[:args].map { |arg| "'#{arg}'" }.sort.join(", ")
-          "brew '#{f[:full_name]}', args: [#{args}]"
-        end
+        brewline = "brew '#{f[:full_name]}'"
+        args = f[:args].map { |arg| "'#{arg}'" }.sort.join(", ")
+        brewline += ", args: [#{args}]" unless f[:args].empty?
+        brewline += BrewServices.started?(f[:full_name]) ? ", service_restart: true" : ''
       end.join("\n")
     end
 
--- lib/bundle/brew_services.rb
+++ lib/bundle/brew_services.rb
@@ -9,5 +9,20 @@ module Bundle
     def self.restart(name)
       Bundle.system "brew", "services", "restart", name
     end
+
+    def self.started?(name)
+      if @started_services.nil?
+        @started_services ||= []
+        @started_names ||= []
+        `brew services list`.lines.grep(/started/).map do |s|
+          s.split(/\s+/).each do |fname, state, user, plist|
+            @started_services << Hash[:name => fname, :user => user, :plist => plist]
+            @started_names << fname
+          end
+        end
+      end
+
+      @started_names.include? name
+    end
   end
 end
