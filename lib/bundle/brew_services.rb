d385c6a06168305041794f8f701b873d289a6a46
Richard Primus <rprimus@gmail.com>
2016-08-10 01:29:28 +0100

    Remove leading "if" decreased coverage

--- lib/bundle/brew_services.rb
+++ lib/bundle/brew_services.rb
@@ -11,18 +11,16 @@ module Bundle
     end
 
     def self.started?(name)
-      if @started_services.nil?
-        @started_services ||= []
-        @started_names ||= []
-        `brew services list`.lines.grep(/started/).map do |s|
-          s.split(/\s+/).each do |fname, state, user, plist|
-            @started_services << Hash[:name => fname, :user => user, :plist => plist]
-            @started_names << fname
-          end
-        end
-      end
-
-      @started_names.include? name
+      @raw_started_services ||= `brew services list`.lines.grep(/started/)
+      @started_services ||= []
+      @started_names ||= []
+      @raw_started_services.map do |s|
+        s.split(/\s+/).each do |fname, state, user, plist|
+          @started_services << Hash[:name => fname, :user => user, :plist => plist]
+          @started_names << fname
+       end
+     end
+     @started_names.include? name
     end
   end
 end
