e430b252886762a829841eaafd78be55f1bba0a0
Colin Dean <git@cad.cx>
2018-08-11 23:36:37 -0400

    Drops unused early exit check from module since it's in the class

--- lib/bundle/checker.rb
+++ lib/bundle/checker.rb
@@ -49,23 +49,6 @@ module Bundle
 
     module_function
 
-    NO_ACTION = [].freeze
-
-    def action_required_for(formula)
-      [formula]
-    end
-
-    def exit_early_check(packages)
-      work_to_be_done = packages.find do |pkg|
-        yield pkg
-      end
-      if work_to_be_done
-        Bundle::Checker.action_required_for(work_to_be_done)
-      else
-        Bundle::Checker::NO_ACTION
-      end
-    end
-
     CheckResult = Struct.new :work_to_be_done, :completed_checks, :errors, :unchecked_checks
 
     CHECKS = {
