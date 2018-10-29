204cf523597e05a0cd2292459cf0f4baca24d878
Colin Dean <git@cad.cx>
2018-10-28 20:03:10 -0400

    Names subject in cask dumper spec

--- spec/bundle/cask_dumper_spec.rb
+++ spec/bundle/cask_dumper_spec.rb
@@ -3,26 +3,24 @@
 require "spec_helper"
 
 describe Bundle::CaskDumper do
-  context "when brew-cask is not installed" do
-    subject { described_class }
+  subject(:dumper) { described_class }
 
+  context "when brew-cask is not installed" do
     before do
       described_class.reset!
       allow(Bundle).to receive(:cask_installed?).and_return(false)
     end
 
     it "returns empty list" do
-      expect(subject.casks).to be_empty
+      expect(dumper.casks).to be_empty
     end
 
     it "dumps as empty string" do
-      expect(subject.dump([])).to eql(["", ""])
+      expect(dumper.dump([])).to eql(["", ""])
     end
   end
 
   context "when there is no cask" do
-    subject { described_class }
-
     before do
       described_class.reset!
       allow(Bundle).to receive(:cask_installed?).and_return(true)
@@ -30,17 +28,15 @@ describe Bundle::CaskDumper do
     end
 
     it "returns empty list" do
-      expect(subject.casks).to be_empty
+      expect(dumper.casks).to be_empty
     end
 
     it "dumps as empty string" do
-      expect(subject.dump([])).to eql(["", ""])
+      expect(dumper.dump([])).to eql(["", ""])
     end
   end
 
   context "cask `foo`, `bar` and `baz` are installed, while `baz` is required by formula" do
-    subject { described_class }
-
     before do
       described_class.reset!
       allow(Bundle).to receive(:cask_installed?).and_return(true)
@@ -48,11 +44,11 @@ describe Bundle::CaskDumper do
     end
 
     it "returns list %w[foo bar baz]" do
-      expect(subject.casks).to eql(%w[foo bar baz])
+      expect(dumper.casks).to eql(%w[foo bar baz])
     end
 
     it "dumps as `cask 'baz'` and `cask 'foo' cask 'bar'`" do
-      expect(subject.dump(%w[baz])).to eql ["cask \"baz\"", "cask \"foo\"\ncask \"bar\""]
+      expect(dumper.dump(%w[baz])).to eql ["cask \"baz\"", "cask \"foo\"\ncask \"bar\""]
     end
   end
 end
