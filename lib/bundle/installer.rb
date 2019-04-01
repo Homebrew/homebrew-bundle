9407f7eb4837558077e69e4ad2ad9a333d5b5dea
Colin Dean <git@cad.cx>
2019-03-31 23:03:33 -0400

    Extracts installer determination to a method

--- lib/bundle/installer.rb
+++ lib/bundle/installer.rb
@@ -4,43 +4,51 @@ module Bundle
   module Installer
     module_function
 
+    InstallerTask = Struct.new :cls, :arg, :verb
+
+    def installer_class_and_args_for_entry(entry)
+      verb = 'Installing'
+      arg = [entry.name]
+      cls = case entry.type
+            when :brew
+              arg << entry.options
+              Bundle::BrewInstaller
+            when :cask
+              arg << entry.options
+              Bundle::CaskInstaller
+            when :mac_app_store
+              arg << entry.options[:id]
+              Bundle::MacAppStoreInstaller
+            when :tap
+              verb = 'Tapping'
+              arg << entry.options
+              Bundle::TapInstaller
+            end
+      InstallerTask.new cls, arg, verb
+    end
+
     def install(entries)
       success = 0
       failure = 0
       errored_entries = {}
 
       entries.each do |entry|
-        arg = [entry.name]
-        verb = "Installing"
-        cls = case entry.type
-        when :brew
-          arg << entry.options
-          Bundle::BrewInstaller
-        when :cask
-          arg << entry.options
-          Bundle::CaskInstaller
-        when :mac_app_store
-          arg << entry.options[:id]
-          Bundle::MacAppStoreInstaller
-        when :tap
-          verb = "Tapping"
-          arg << entry.options
-          Bundle::TapInstaller
-        end
+        task = installer_class_and_args_for_entry(entry)
+        task_header = "#{task.verb} #{entry.name}"
         begin
-          case cls.install(*arg)
+          case task.cls.install(*task.arg)
           when :success
-            puts Formatter.success("#{verb} #{entry.name}")
+            puts Formatter.success(task_header)
             success += 1
           when :skipped
             puts "Using #{entry.name}"
             success += 1
           else
-            puts Formatter.error("#{verb} #{entry.name} has failed!")
+            puts Formatter.error("#{task_header} has failed!")
             failure += 1
           end
         rescue => e
-          puts Formatter.error("#{verb} #{entry.name} raised an exception: #{e}")
+          puts Formatter.error("#{task_header} raised an exception: #{e}")
           errored_entries[entry.name] = e
         end
       end
