3290c22036f9e113dfe9a6ddbc900d0781cea7f3
Jacob Bednarz <jacob.bednarz@gmail.com>
2020-03-11 10:07:44 +1100

    No need to append options to the installer, we just want the name

--- lib/bundle/installer.rb
+++ lib/bundle/installer.rb
@@ -22,7 +22,6 @@ module Bundle
           arg << entry.options[:id]
           Bundle::MacAppStoreInstaller
         when :whalebrew
-          arg << entry.options
           Bundle::WhalebrewInstaller
         when :tap
           verb = "Tapping"
