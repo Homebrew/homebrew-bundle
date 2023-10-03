6190c35d2e8086a3c94c391d24fed0bceda1ce3e
Colin Dean <colindean@users.noreply.github.com>
2023-10-03 00:06:25 +0000

    Adds a devcontainer primarily for GitHub Codespaces use
    This will enable faster configuration of a development environment.
    
    This is based off of https://github.com/Homebrew/brew/tree/4.1.14/.devcontainer

---
+++ .devcontainer/on-create-command.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -eux -o pipefail
+
+# fix permissions so Homebrew and Bundler don't complain
+sudo chmod -R g-w,o-w /home/linuxbrew
+
+# everything below is too slow to do unless prebuilding so skip it
+CODESPACES_ACTION_NAME="$(jq --raw-output '.ACTION_NAME' /workspaces/.codespaces/shared/environment-variables.json)"
+if [[ "${CODESPACES_ACTION_NAME}" != "createPrebuildTemplate" ]]
+then
+  echo "Skipping slow items, not prebuilding."
+  exit 0
+fi
+
+# install Homebrew's development gems
+brew install-bundler-gems --groups=all
+
+# install Homebrew formulae we might need
+# brew install shellcheck shfmt gh gnu-tar
+
+# cleanup any mess
+brew cleanup
+
+# actually tap homebrew/core, no longer done by default
+# brew tap homebrew/core
+
+# install some useful development things
+sudo apt-get update
+
+apt_get_install() {
+  sudo apt-get install --yes --no-install-recommends \
+    -o Dpkg::Options::=--force-confdef \
+    -o Dpkg::Options::=--force-confnew \
+    "$@"
+}
+
+apt_get_install \
+  openssh-server \
+  zsh
+
+# Ubuntu 18.04 doesn't include zsh-autosuggestions
+if ! grep -q "Ubuntu 18.04" /etc/issue &>/dev/null
+then
+  apt_get_install zsh-autosuggestions
+fi
+
+# Start the SSH server so that `gh cs ssh` works.
+sudo service ssh start
