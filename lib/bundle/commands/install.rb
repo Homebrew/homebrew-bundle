9242457df5af7b05adbda1439904010e23ead1e2
Mike McQuaid <mike@mikemcquaid.com>
2024-06-07 10:14:39 +0100

    Fix `brew bundle --cleanup` from `stdin`
    This was previously failing to read a valid `Brewfile` on repeated reads
    so attempting to cleanup everything.
    
    Instead, cache the `Brewfile` DSL in the `install` command and pass it
    to the `cleanup` command.
    
    Fixes #1378

--- lib/bundle/commands/install.rb
+++ lib/bundle/commands/install.rb
@@ -6,11 +6,15 @@ module Bundle
       module_function
 
       def run(global: false, file: nil, no_lock: false, no_upgrade: false, verbose: false, force: false, quiet: false)
-        parsed_entries = Brewfile.read(global:, file:).entries
+        @dsl = Brewfile.read(global:, file:)
         Bundle::Installer.install(
-          parsed_entries,
+          @dsl.entries,
           global:, file:, no_lock:, no_upgrade:, verbose:, force:, quiet:,
         ) || exit(1)
+      end
+
+      def dsl
+        @dsl
       end
     end
   end
