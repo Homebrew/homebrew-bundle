b55659f849baa70f1593b88fb57fddeeb4c7a855
Eric Knibbe <enk3@outlook.com>
2024-11-06 10:15:40 -0500

    add missing OpenStruct requires

--- spec/bundle/brew_dumper_spec.rb
+++ spec/bundle/brew_dumper_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "ostruct"
 require "spec_helper"
 require "tsort"
 require "formula"
--- spec/bundle/skipper_spec.rb
+++ spec/bundle/skipper_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "ostruct"
 require "spec_helper"
 
 describe Bundle::Skipper do
