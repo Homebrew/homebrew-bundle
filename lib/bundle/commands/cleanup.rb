8663a3fd8b99fd07db2ac1d0a3c0bcbb9f5458ed
Troels Thomsen <troels@thomsen.io>
2024-11-24 08:38:05 +0100

    Extract method

--- lib/bundle/commands/cleanup.rb
+++ lib/bundle/commands/cleanup.rb
@@ -153,11 +153,15 @@ module Bundle
 
       def taps_to_untap(global: false, file: nil)
         @dsl ||= Brewfile.read(global:, file:)
-        kept_formulae = self.kept_formulae(global:, file:).map(&Formulary.method(:factory))
+        kept_formulae = self.kept_formulae(global:, file:).map(&method(:lookup_formula))
         kept_taps = @dsl.entries.select { |e| e.type == :tap }.map(&:name)
         kept_taps += kept_formulae.filter_map(&:tap).map(&:name)
         current_taps = Bundle::TapDumper.tap_names
         current_taps - kept_taps - IGNORED_TAPS
+      end
+
+      def lookup_formula(formula)
+        Formulary.factory(formula)
       end
 
       def vscode_extensions_to_uninstall(global: false, file: nil)
