cdac4b03b0339efc86e85101fbc4881f7bd47353
Mike McQuaid <mike@mikemcquaid.com>
2025-03-06 15:22:29 +0000

    Improve `brew bundle env --install` output
    Use `$stderr` for `brew bundle env --install` output that we don't want
    to be e.g. `eval`ed.

--- lib/bundle/commands/install.rb
+++ lib/bundle/commands/install.rb
@@ -6,11 +6,12 @@ module Bundle
     module Install
       module_function
 
-      def run(global: false, file: nil, no_lock: false, no_upgrade: false, verbose: false, force: false, quiet: false)
+      def run(global: false, file: nil, no_lock: false, no_upgrade: false, verbose: false, force: false,
+              output: $stdout, quiet: false)
         @dsl = Brewfile.read(global:, file:)
         Bundle::Installer.install(
           @dsl.entries,
-          global:, file:, no_lock:, no_upgrade:, verbose:, force:, quiet:,
+          global:, file:, no_lock:, no_upgrade:, verbose:, force:, output:, quiet:,
         ) || exit(1)
       end
 
